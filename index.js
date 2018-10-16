8c1f6320cc60f3881167f93ead4ef045d0e55503
florah001 <florah001@yahoo.co.uk>
2018-10-16 07:24:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,40 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({}, object, key);
+}
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObj = Object.assign({},(object, 'prop2', 2));
+  return  { prop: 1, prop2: 2 };
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  var obj = { prop: 1 }
+  var newObj = deleteFromObjectByKey(obj, 'prop')
+  return  { prop: 1, prop2: 2 };
+}
+function deleteFromObjectByKey(object, key) {
+  var obj = { prop: 1 }
+  var newObj = Object.assign({}, object)
+   delete prop[1];
+    return obj;
+}
+function deleteFromObjectByKey(object, key) {
+   delete object[key];
+    return object;    
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+        return object;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
+
+
+
+/*
+
+    
+  1) Objects updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
+     ReferenceError: updateObjectWithKeyAndValue is not defined
+*/
